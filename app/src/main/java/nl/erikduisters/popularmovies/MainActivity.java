7f7eff26eaa6a94ca1f3313cb491df9ed5c5c956
Erik Duisters <e.duisters@outlook.com>
2018-02-16 17:08:14 +0100

    Added main_activity options menu

--- app/src/main/java/nl/erikduisters/popularmovies/MainActivity.java
+++ app/src/main/java/nl/erikduisters/popularmovies/MainActivity.java
@@ -3,13 +3,15 @@ package nl.erikduisters.popularmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import timber.log.Timber;
 
-//TODO: Add about screen and give credit to TMDB as required
 //TODO: Add sort order to menu
 //TODO: Make sure app does not crash when there is no internet connection
 public class MainActivity extends AppCompatActivity {
@@ -36,5 +38,28 @@ public class MainActivity extends AppCompatActivity {
         if (unbinder != null) {
             unbinder.unbind();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                //TODO: Show about dialog that gives credit to TMDB
+                Timber.d("About was clicked");
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
