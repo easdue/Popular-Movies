3e146c68320e71732374ddd528e93fdee0a14e04
Erik Duisters <e.duisters@outlook.com>
2018-02-16 15:49:59 +0100

    Added MyApplication.java to initialize Fresco and Timber

---
+++ app/src/main/java/nl/erikduisters/popularmovies/MyApplication.java
@@ -0,0 +1,32 @@
+package nl.erikduisters.popularmovies;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import timber.log.Timber;
+
+/**
+ * Created by Erik Duisters on 16-02-2018.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Fresco.initialize(this);
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new ReleaseTree());
+        }
+    }
+
+    private static class ReleaseTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+        }
+    }
+}
