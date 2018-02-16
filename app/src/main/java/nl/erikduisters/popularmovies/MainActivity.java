dcc52621684e1222496035e25e02986ef9d9c170
Erik Duisters <e.duisters@outlook.com>
2018-02-16 16:56:15 +0100

    Added design support library as dependency Changed activity_main layout

--- app/src/main/java/nl/erikduisters/popularmovies/MainActivity.java
+++ app/src/main/java/nl/erikduisters/popularmovies/MainActivity.java
@@ -2,12 +2,39 @@ package nl.erikduisters.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+//TODO: Add about screen and give credit to TMDB as required
+//TODO: Add sort order to menu
+//TODO: Make sure app does not crash when there is no internet connection
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.fragmentPlaceholder) FrameLayout fragmentPlaceholder;
+
+    private Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        unbinder = ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
     }
 }
