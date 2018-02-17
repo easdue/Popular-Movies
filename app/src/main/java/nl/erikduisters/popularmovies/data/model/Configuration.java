81e9a033c0f18ca1ad473fa88e54ee396a935cf9
Erik Duisters <e.duisters@outlook.com>
2018-02-17 15:28:55 +0100

    Created Model class Configuration

---
+++ app/src/main/java/nl/erikduisters/popularmovies/data/model/Configuration.java
@@ -0,0 +1,26 @@
+package nl.erikduisters.popularmovies.data.model;
+
+import java.util.List;
+
+/**
+ * Created by Erik Duisters on 17-02-2018.
+ */
+
+public class Configuration {
+    private Images images;
+    private List<String> changeKeys;
+
+    public Configuration() {}
+
+    public static class Images {
+        private String baseUrl;
+        private String secureBaseUrl;
+        private List<String> backdropSizes;
+        private List<String> logoSizes;
+        private List<String> posterSizes;
+        private List<String> profileSizes;
+        private List<String> stillSizes;
+
+        public Images() {}
+    }
+}
